7e98dafbff4dfa3f28d59c792b2590515b895b8c
高橋風宇多 <takahashifuta@takahashifuuutanoMacBook-Pro.local>
2020-12-02 16:19:47 +0900

    ルーティングを実装

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import express from 'express'
 import helmet from 'helmet'
 import cors from 'cors'
 import bodyParser from 'body-parser'
+import { createUser, deleteUser, listUsers } from './services/UserService'
 
 const app = express()
 app.use(helmet())
@@ -17,8 +18,22 @@ app.use(bodyParser.json())
 /* → ここからRoutes */
 
 // GET /api/users → ユーザー一覧を返す
+app.get('/api/users', async (req, res) => {
+  res.send(await listUsers())
+})
 // POST /api/users → ユーザーを作成する(req.data から email と password を抜き出して保存)
+app.post('/api/users', async (req, res) => {
+  await createUser({
+    email: req.body.email,
+    password: req.body.password
+  })
+  res.send()
+})
 // DELETE /api/users/:id → ユーザーを削除する（:idのユーザーを削除する）
+app.delete('/api/users', async (req, res) => {
+  await deleteUser(req.body.id)
+  res.send()
+})
 
 /* ← ここまでRoutes */
 
