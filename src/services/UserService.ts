e17faa511330d1745d59b3ded0c37725d5d45236
hota1024 <hotalog@hota1024.com>
2020-12-02 15:12:14 +0900

    🏷️ add listUser and deleteUser

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -13,3 +13,25 @@ export const createUser = async (data: Partial<User>): Promise<User> => {
 
   return await repository.save(user)
 }
+
+/**
+ * list users.
+ */
+export const listUsers = async (): Promise<User[]> => {
+  const repository = await getRepository(User)
+
+  return await repository.find()
+}
+
+/**
+ * delete a user.
+ *
+ * @param id user id.
+ */
+export const deleteUser = async (id: number): Promise<void> => {
+  const repository = await getRepository(User)
+
+  repository.delete({ id })
+
+  return
+}
