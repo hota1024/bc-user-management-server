c8bbbdeb24118fbf17c6de258c10e82f6b46e47b
hota1024 <hotalog@hota1024.com>
2020-12-02 15:05:53 +0900

    🏷️ Add index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log('hello,world')
+import 'reflect-metadata'
+
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+import express from 'express'
+import helmet from 'helmet'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+
+const app = express()
+app.use(helmet())
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+/* → ここからRoutes */
+
+/* ← ここまでRoutes */
+
+app.listen(process.env.APP_PORT, () => {
+  console.log(`🚀 listen on port ${process.env.APP_PORT}`)
+})
